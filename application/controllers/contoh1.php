b76647d71c5d2d8d3d0a08fd3ff5618a99b73bb5
najmahsharfinaj <najmahsharfinajihadi@gmail.com>
2023-09-25 15:57:38 +0700

    menambahkan wp2 tugas 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Najmah Sharfina Jihadi
+        Saya tinggal di daerah cibarusah 
+        Olahraga yang saya sukai adalah Berenang";
+    }
+}
