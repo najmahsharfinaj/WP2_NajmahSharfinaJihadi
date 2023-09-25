5567fa67515c99321977ac71f9171f62ca10a80a
najmahsharfinaj <najmahsharfinajihadi@gmail.com>
2023-09-25 16:18:28 +0700

    menambahkan Latihan1

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,15 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>BIODATA</h1>";
+        echo "Nama   : Najmah Sharfina JIhadi
+        <p>Nim     : 19220875</p>
+        <p>Kelas    : 19.3A.24</p>
+        <p>No. telp : 083194749715</p>
+        <p>Hobi     : Fangirling</p>
+        <p>Alamat   : perumahan cibarusah indah</p>
+        <p>Umur     : 19</p>";   
+    }
+}
