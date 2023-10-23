31dab46293cf389ec69707c35bfab1501387b1c0
najmahsharfinaj <najmahsharfinajihadi@gmail.com>
2023-10-23 12:52:05 +0700

    tugas pertemuan 3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -3,13 +3,17 @@ class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1>BIODATA</h1>";
-        echo "Nama   : Najmah Sharfina JIhadi
-        <p>Nim     : 19220875</p>
-        <p>Kelas    : 19.3A.24</p>
-        <p>No. telp : 083194749715</p>
-        <p>Hobi     : Fangirling</p>
-        <p>Alamat   : perumahan cibarusah indah</p>
-        <p>Umur     : 19</p>";   
+        echo "Selamat Datang.. selamat belajar web progamming";
+        //$this->load->view('view-latihan1');   
+    }
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+        
+        $this->load->view('view-latihan1', $data);
     }
 }
---
+++ application/controllers/contoh1model.php
@@ -0,0 +1,8 @@
+<?php
+class Latihan1 extends C1_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar web progamming";
+    }
+}
